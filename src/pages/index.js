43dbe06b8ee0692f4128af2d3cc2ad5738507409
Daniel Klava <klava.daniel@gmail.com>
2019-07-24 10:40:36 -0300

    Versão preliminar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,87 @@
-import React from "react"
+import React from 'react'
+import Helmet from 'react-helmet'
+
+import Layout from '../components/layout'
+
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexPage extends React.Component {
+
+	constructor() {
+		super();
+	}
+
+	render() {
+		const siteTitle = "Gatsby Starter - Strata"
+		const siteDescription = "Site description"
+
+		return (
+			<Layout>
+				<Helmet>
+					<title>{siteTitle}</title>
+					<meta name="description" content="{siteDescription}" />
+				</Helmet>
+				<div id="main">
+
+					<section id="one">
+						<header className="major">
+							<h2>Ipsum lorem dolor aliquam ante commodo<br />
+								magna sed accumsan arcu neque.</h2>
+						</header>
+						<p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
+						<ul className="actions">
+							<li><a href="#" className="button">Learn More</a></li>
+						</ul>
+					</section>
+
+					<section id="two">
+						<h2>Recent Work</h2>
+
+					</section>
+
+					<section id="three">
+						<h2>Get In Touch</h2>
+						<p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
+						<div className="row">
+							<div className="8u 12u$(small)">
+								<form method="post" action="#">
+									<div className="row uniform 50%">
+										<div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
+										<div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
+										<div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
+									</div>
+								</form>
+								<ul className="actions">
+									<li><input type="submit" value="Send Message" /></li>
+								</ul>
+							</div>
+							<div className="4u 12u$(small)">
+								<ul className="labeled-icons">
+									<li>
+										<h3 className="icon fa-home"><span className="label">Address</span></h3>
+										1234 Somewhere Rd.<br />
+										Nashville, TN 00000<br />
+										United States
+									</li>
+									<li>
+										<h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
+										000-000-0000
+									</li>
+									<li>
+										<h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
+										<a href="#">hello@untitled.tld</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</section>
+
+				</div>
+			</Layout>
+		)
+	}
+}
 
 export default IndexPage
